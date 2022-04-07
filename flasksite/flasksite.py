b516efaae614994d99c8dea8083d424b7fab79b6
lucas-su <lucas.schootuiterkamp@gmail.com>
2022-04-07 10:53:00 +0200

    mvp

--- flasksite/flasksite.py
+++ flasksite/flasksite.py
@@ -2,22 +2,16 @@ from flask import Flask, render_template, request, make_response
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, IntegerField, RadioField
 from wtforms.validators import DataRequired, NumberRange
-import os
-from flask_bootstrap import Bootstrap
-import pandas
-import datetime
+from flask_bootstrap import Bootstrap5
 from waitress import serve
-
 import mysql.connector
 import os, json
 
 app = Flask(__name__)
-Bootstrap(app)
+Bootstrap5(app)
 
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
-
-backupcounter = 0
 
 def condCreateDir(dir):
     """
@@ -34,33 +28,81 @@ class annotatorNameForm(FlaskForm):
     submitName = SubmitField('Save and continue')
 
 
-class annotationForm(FlaskForm):
-    annotatorName = StringField("Username:", validators=[DataRequired()])
-    aff_roll = BooleanField('should be rolled')
-    aff_fragile = BooleanField('should be handled with care')
-    aff_stack = BooleanField('can be stacked (onto)')
-    aff_grasp = BooleanField('can be grasped')
-    aff_cut_scoop = BooleanField('can serve to cut or scoop')
-    aff_support = BooleanField('can support other objects')
-    aff_wrap = BooleanField('can be wrapped in a hand')
-    aff_push = BooleanField('should be pushed')
-    aff_drag = BooleanField('should be dragged')
-    aff_carry = BooleanField('can be carried')
-    aff_open = BooleanField('can be opened')
-    aff_pour = BooleanField('can serve to pour something out of')
-    aff_observe = BooleanField('can be observed')
-    aff_hit = BooleanField('can be used to hit something')
-    aff_none = BooleanField('affords no interaction')
-    aff_pull = BooleanField('has parts that can be pulled')
-    aff_tip = BooleanField('has buttons that can be pressed')
-    aff_warmth = BooleanField('gives off warmth')
-    aff_illumination = BooleanField('illuminates surroundings')
-    aff_walk = BooleanField('can be walked on')
-    aff_move = BooleanField('can be moved')
-    aff_no_clue = BooleanField('Misspelled word/don\'t know')
+def annotationForms():
+    high_level = {"con_move": "can be moved in a constrained manner",
+                  "uncon_move": "can be moved in an unconstrained manner",
+                  "dir_affs": "object can be interacted with directly",
+                  "indir_affs": "object can interact with  other objects",
+                  "observe_affs": "object are made for looking at",
+                  "no_affs": "object does not readily offer interaction"}
 
-    currentObject = StringField()
-    submit = SubmitField('Submit')
+    con_move = {"roll": ["can be rolled", "eg. trolley"],
+                "push": ["should be pushed", 'eg. pram'],
+                "drag": ["should be dragged", "eg. rubbish bin"]
+                }
+
+    uncon_move = {"carry": ["can be carried", "small objects eg. pen, book"],
+                  "pour": ["can serve to pour something out of", "eg. cup, flask"]
+                  }
+
+    dir_affs = {"fragile": ["should be handled with care", "breakable items eg. glass vase"],
+                "open": ["can be opened", "eg. cupboard, bottle"],
+                "grasp": ["can be grasped", "small objects eg. phone or objects with handles"],
+                "pull": ["can be pulled", "eg. handle, knob"],
+                "tip": ["has buttons that can be pressed", "eg. light switch, buttons on device"]
+                }
+
+    indir_affs ={"stack": ["can be stacked (onto)", "eg. plates, stackable chairs"],
+                 "cut_scoop": ["can serve to cut or scoop", "eg. knife, spoon, scoop"],
+                 "support": ["can support other objects", "surfaces which can you can put something on, eg. desk, table"],
+                 "hit": ["can be used to hit something", "eg. hammer"]
+                 }
+
+    observe_affs = {"info": ["displays information", "eg. screens, information poster"],
+                    "deco": ["serves as decoration", "eg. painting"]
+                    }
+
+    no_affs = {"none": ["affords no interaction", "eg. wall, floor"],
+               "warmth": ["gives off warmth", "eg. fireplace, heater"],
+               "illumination": ["illuminates surroundings", "eg. lamp"],
+               "walk": ["can be walked on", "eg. floor"]
+               }
+
+    no_clue_aff = {"no_clue": "Misspelled word/meaning of the word unclear"}
+
+    return [high_level, no_clue_aff, con_move, uncon_move, dir_affs, indir_affs, observe_affs, no_affs]
+
+
+
+    #
+    # annotatorName = StringField("Username:", validators=[DataRequired()])
+    # aff_roll = BooleanField('can be rolled', description="description")
+    # aff_fragile = BooleanField('should be handled with care')
+    # aff_stack = BooleanField('can be stacked (onto)')
+    # aff_grasp = BooleanField('can be grasped')
+    # aff_cut_scoop = BooleanField('can serve to cut or scoop')
+    # aff_support = BooleanField('can support other objects')
+    # aff_wrap = BooleanField('can be wrapped in a hand')
+    # aff_push = BooleanField('should be pushed')
+    # aff_drag = BooleanField('should be dragged')
+    # aff_carry = BooleanField('can be carried')
+    # aff_open = BooleanField('can be opened')
+    # aff_pour = BooleanField('can serve to pour something out of')
+    # aff_observe = BooleanField('can be observed')
+    # aff_hit = BooleanField('can be used to hit something')
+    # aff_none = BooleanField('affords no interaction')
+    # aff_pull = BooleanField('has parts that can be pulled')
+    # aff_tip = BooleanField('has buttons that can be pressed')
+    # aff_warmth = BooleanField('gives off warmth')
+    # aff_illumination = BooleanField('illuminates surroundings')
+    # aff_walk = BooleanField('can be walked on')
+    # aff_move = BooleanField('can be moved')
+    # aff_no_clue = BooleanField('Misspelled word/meaning unclear')
+
+
+
+    # currentObject = StringField()
+    # submit = SubmitField('Submit')
 
 
 def saveAnnotation(form):
@@ -171,21 +213,24 @@ def getNewPosts(name):
 @app.route('/', methods=['GET', 'POST'])
 def main():
     nameForm = annotatorNameForm()
-    form = annotationForm()
+    forms = annotationForms()
     name = request.cookies.get("annotatorName")
 
-    if form.validate_on_submit():
-        saveAnnotation(form)
-        # name = form.annotatorName.data
-        currentObject = getNewPosts(name)
-    elif name:
-        currentObject = getNewPosts(name)
-    elif nameForm.validate_on_submit():
-        name = nameForm.annotatorNamefield.data
-        currentObject = getNewPosts(name)
-    else:
-        return render_template("annotatorInfo.html", form=annotatorNameForm())
-    resp = make_response(render_template("annotatorInfo.html", form=annotationForm(), currentObject=currentObject, name=name))
+    # if form.validate_on_submit():
+    #     saveAnnotation(form)
+    #     # name = form.annotatorName.data
+    #     currentObject = getNewPosts(name)
+    # elif name:
+    #     currentObject = getNewPosts(name)
+    # elif nameForm.validate_on_submit():
+    #     name = nameForm.annotatorNamefield.data
+    #     currentObject = getNewPosts(name)
+    # else:
+    #     return render_template("annotatorInfo.html", form=annotatorNameForm())
+
+    currentObject = getNewPosts(name)
+
+    resp = make_response(render_template("annotatorInfo.html", forms=forms, currentObject=currentObject, name=name, zip=zip))
     resp.set_cookie("annotatorName", name)
     return resp
 
