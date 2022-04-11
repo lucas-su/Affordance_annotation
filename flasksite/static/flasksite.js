452a9ac00d59beb27bf5e30ca27a1f53d1c0dc49
lucas-su <lucas.schootuiterkamp@gmail.com>
2022-04-11 16:18:59 +0200

    submit button behaviour + message

--- flasksite/static/flasksite.js
+++ flasksite/static/flasksite.js
@@ -1,50 +1,72 @@
+function enable_disable_submit(){
+    if(document.getElementById('no_clue').checked ||
+    document.getElementById('con_move').checked ||
+    document.getElementById('uncon_move').checked ||
+    document.getElementById('dir_affs').checked ||
+    document.getElementById('indir_affs').checked ||
+    document.getElementById('observe_affs').checked ||
+    document.getElementById('social_affs').checked ||
+    document.getElementById('no_affs').checked
+    ){
+    document.getElementById('main_form_submit').disabled = false;
+    document.getElementById('submit_message').hidden = true;
+    } else{
+    document.getElementById('submit_message').hidden = false;
+    document.getElementById('main_form_submit').disabled = true;
+    }
+}
+
 $(".con_move").change(function(){
     if(this.checked) {
     document.getElementById('con_move').checked = true;
     }
+    enable_disable_submit();
 });
 
 $(".uncon_move").change(function(){
     if(this.checked) {
     document.getElementById('uncon_move').checked = true;
     }
+    enable_disable_submit();
 });
 
 $(".dir_affs").change(function(){
     if(this.checked) {
     document.getElementById('dir_affs').checked = true;
     }
+    enable_disable_submit();
 });
 
 $(".indir_affs").change(function(){
     if(this.checked) {
     document.getElementById('indir_affs').checked = true;
     }
+    enable_disable_submit();
 });
 
 $(".observe_affs").change(function(){
     if(this.checked) {
     document.getElementById('observe_affs').checked = true;
     }
+    enable_disable_submit();
 });
 
 $(".social_affs").change(function(){
     if(this.checked) {
     document.getElementById('social_affs').checked = true;
     }
+    enable_disable_submit();
 });
 
 $(".no_affs").change(function(){
     if(this.checked) {
     document.getElementById('no_affs').checked = true;
     }
+    enable_disable_submit();
 });
 
-
-
-
-
 $(document.getElementById('con_move')).change(function(){
+    enable_disable_submit();
     if(this.checked == false){
     $(".con_move").each(function(){
         this.checked = false;
@@ -53,6 +75,7 @@ $(document.getElementById('con_move')).change(function(){
 });
 
 $(document.getElementById('uncon_move')).change(function(){
+    enable_disable_submit();
     if(this.checked == false){
     $(".uncon_move").each(function(){
         this.checked = false;
@@ -61,6 +84,7 @@ $(document.getElementById('uncon_move')).change(function(){
 });
 
 $(document.getElementById('dir_affs')).change(function(){
+    enable_disable_submit();
     if(this.checked == false){
     $(".dir_affs").each(function(){
         this.checked = false;
@@ -69,6 +93,7 @@ $(document.getElementById('dir_affs')).change(function(){
 });
 
 $(document.getElementById('indir_affs')).change(function(){
+    enable_disable_submit();
     if(this.checked == false){
     $(".indir_affs").each(function(){
         this.checked = false;
@@ -77,6 +102,7 @@ $(document.getElementById('indir_affs')).change(function(){
 });
 
 $(document.getElementById('observe_affs')).change(function(){
+    enable_disable_submit();
     if(this.checked == false){
     $(".observe_affs").each(function(){
         this.checked = false;
@@ -85,6 +111,7 @@ $(document.getElementById('observe_affs')).change(function(){
 });
 
 $(document.getElementById('social_affs')).change(function(){
+    enable_disable_submit();
     if(this.checked == false){
     $(".social_affs").each(function(){
         this.checked = false;
@@ -93,6 +120,7 @@ $(document.getElementById('social_affs')).change(function(){
 });
 
 $(document.getElementById('no_affs')).change(function(){
+    enable_disable_submit();
     if(this.checked == false){
     $(".no_affs").each(function(){
         this.checked = false;
@@ -100,7 +128,23 @@ $(document.getElementById('no_affs')).change(function(){
     }
 });
 
+$(document.getElementById('main_form_submit')).click(function(){
+    if(document.getElementById('no_clue').checked ||
+    document.getElementById('con_move').checked ||
+    document.getElementById('uncon_move').checked ||
+    document.getElementById('dir_affs').checked ||
+    document.getElementById('indir_affs').checked ||
+    document.getElementById('observe_affs').checked ||
+    document.getElementById('social_affs').checked ||
+    document.getElementById('no_affs').checked
+    ){
+    document.getElementById('mainform').submit();
+    }
+});
 
+$(document).ready(function() {
+    document.getElementById('main_form_submit').disabled = true;
+});
 
 
 //$(document).ready(function() {
